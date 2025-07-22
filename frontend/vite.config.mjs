b900168c6b0ff467058fc55f4c62d8ed6925b2d3
Maxwell Klema <80615123+maxklema@users.noreply.github.com>
2025-07-22 12:59:47 -0400

    Update vite.config.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:5000',
     },
-    allowedHosts: 'polyglot-test-maxklema-test-branch-1.opensource.mieweb.org', // Allow all hosts
+    allowedHosts: 'polyglot-test-maxklema-main.opensource.mieweb.org', // Allow all hosts
   },
   build: {
     outDir: 'dist',
