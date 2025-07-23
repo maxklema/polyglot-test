e068119fe90fd4ff02ad8aa2853d21775574f1ad
Maxwell Klema <80615123+maxklema@users.noreply.github.com>
2025-07-23 15:21:45 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ CORS(app)  # Enable CORS for development
 
 @app.route("/api/message")
 def get_message():
-    return jsonify({"message": "Hello from Flask!!! If you see this then it updated."})
+    return jsonify({"message": "Hello from Flask!!! Is this thing on?"})
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
